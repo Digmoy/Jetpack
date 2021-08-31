7f63604a0b61eee238c79b7876371bd42fb9899c
Digmoy Saha <digmoy.saha@techprostudio.com>
2021-08-31 22:36:26 +0530

    31 aug

---
+++ app/src/main/java/com/example/jetpacktutorial/DemoOneActivity.kt
@@ -0,0 +1,29 @@
+package com.example.jetpacktutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.jetpacktutorial.databinding.ActivityDemoOneBinding
+
+class DemoOneActivity : AppCompatActivity() {
+
+    private val binding : ActivityDemoOneBinding by lazy {
+        DataBindingUtil.setContentView(this,R.layout.activity_demo_one)
+    }
+
+   // private lateinit var binding : ActivityDemoOneBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+      //  binding = DataBindingUtil.setContentView(this,R.layout.activity_demo_one)
+        binding.btnSubmit.setOnClickListener {
+            displayName()
+        }
+    }
+
+    private fun displayName() {
+        binding.apply {
+            userName.text = "Hello! "+name.text
+        }
+    }
+}
